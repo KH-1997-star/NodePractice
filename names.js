963ef11231883c3f238d550947f3427720741d27
KH-1997-star <gticomando@gmail.com>
2022-06-21 08:41:51 +0100

    first commit

---
+++ names.js
@@ -0,0 +1,7 @@
+//Local
+const jhon = "jhon",
+  //Share
+  peter = "peter",
+  secret = "Super Secret";
+
+module.exports = { jhon, peter };
---
+++ utils.js
@@ -0,0 +1,8 @@
+const sayHi = (name) => {
+  console.log(`helle there ${name}`);
+};
+
+const sum = (a, b) => {
+  console.log(a + b);
+};
+module.exports = { sayHi, sum };
